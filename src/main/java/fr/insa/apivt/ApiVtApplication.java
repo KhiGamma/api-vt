2d1736c2d010c80e9d1b4064c42fc3072e4d4dd9
Antoine <antbury28@gmail.com>
2023-01-18 11:04:41 +0100

    Initial commit

---
+++ src/main/java/fr/insa/apivt/ApiVtApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.apivt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVtApplication.class, args);
+	}
+
+}
