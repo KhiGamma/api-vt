48f9f7c4254dc5e9e2163ebf710cb8295897a7f8
Antoine <antbury28@gmail.com>
2023-02-16 14:12:18 +0100

    get les séances par dates croissante

--- src/main/java/fr/insa/apivt/repositories/SeancesRepository.java
+++ src/main/java/fr/insa/apivt/repositories/SeancesRepository.java
@@ -22,20 +22,21 @@ public interface SeancesRepository extends JpaRepository<Seances, Integer> {
     List<Seances> getAllSeances(Pageable pageable);
 
     @Query(value = "SELECT s.codeSeance, s.dateSeance, s.heureSeance, ens.nom nomEns, s.commentaire, rsp.codeProf, rsp.nom nomProf, dip.codeDiplome, dip.nom nomDiplome, dip.codeResponsable, rsg.codeGroupe, rsg.nom nomGroupe, rss.nom nomSalle " +
-            "FROM seances as s, seances_profs as sp, ressources_profs as rsp, enseignements as ens, diplomes as dip, seances_groupes as sg, ressources_groupes as rsg, seances_salles as ss, ressources_salles as rss " +
+            "FROM seances as s, seances_profs AS sp, ressources_profs AS rsp, enseignements AS ens, diplomes AS dip, seances_groupes AS sg, ressources_groupes AS rsg, seances_salles AS ss, ressources_salles AS rss " +
             "WHERE rsp.codeprof = :codeProf " +
-            "and s.dateSeance >= :dateAuPlusTot " +
-            "and sp.codeRessource = rsp.codeProf " +
-            "and sp.codeSeance = s.codeSeance " +
-            "and s.codeEnseignement = ens.codeEnseignement " +
-            "and dip.codeDiplome = ens.codeDiplome " +
-            "and sg.codeSeance = s.codeSeance " +
-            "and sg.codeRessource = rsg.codeGroupe " +
-            "and ss.codeSeance = s.codeseance " +
-            "and ss.codeRessource = rss.codeSalle " +
-            "and s.deleted = 0 " +
-            "and sp.deleted = 0 " +
-            "and sg.deleted = 0 " +
-            "and ss.deleted = 0 ", nativeQuery = true)
+            "AND s.dateSeance >= :dateAuPlusTot " +
+            "AND sp.codeRessource = rsp.codeProf " +
+            "AND sp.codeSeance = s.codeSeance " +
+            "AND s.codeEnseignement = ens.codeEnseignement " +
+            "AND dip.codeDiplome = ens.codeDiplome " +
+            "AND sg.codeSeance = s.codeSeance " +
+            "AND sg.codeRessource = rsg.codeGroupe " +
+            "AND ss.codeSeance = s.codeseance " +
+            "AND ss.codeRessource = rss.codeSalle " +
+            "AND s.deleted = 0 " +
+            "AND sp.deleted = 0 " +
+            "AND sg.deleted = 0 " +
+            "AND ss.deleted = 0 " +
+            "ORDER BY s.dateSeance ASC ", nativeQuery = true)
     List<SeancesProfDto> getSeancesOfProf(@Param("codeProf") Integer codeProf, @Param("dateAuPlusTot") Date dateAuPlusTot);
 }
